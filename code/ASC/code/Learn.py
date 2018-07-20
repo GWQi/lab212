6a98fa926969e121c99a514b8806d4ad2a2ab7ed
guwenqi <guwenqi936@gmail.com>
2018-07-19 20:39:39 -0700

    Please enter the commit message for your changes. Lines starting

--- code/ASC/code/Learn.py
+++ code/ASC/code/Learn.py
@@ -502,11 +502,11 @@ class Train(builtins.object):
                                                'lster'])
 
 
-        # if not self._with_statistics in ['True', 'true']:
-        #     self._write_all_statistics()
+        if not self._with_statistics in ['True', 'true']:
+            self._write_all_statistics()
         
-        # self._write_feature_ranking()
-        # self._write_cross_validation()
+        self._write_feature_ranking()
+        self._write_cross_validation()
         self.pop_system()
 
 
