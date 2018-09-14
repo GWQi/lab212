8dac156db95f4de753984a8dfbc08d2c4589df20
guwenqi <hey_xiaoqi@163.com>
2018-09-14 00:51:52 -0700

    change the structure of ASC directory

---
+++ code/ASC/ASC/code/tools/preprocess.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# *************************************************
+# Author: guwenqi
+# Last modified:
+# Email: hey_xiaoqi@163.com
+# Filename: preprocess.py
+# Description: music/speech data preprocession 
+# *************************************************
+
+def syspath():
+  """
+  append this project to system path
+  """
+  project_root = 
+
+def main(musicroot, speechroot, fearoot):
---
+++ code/ASC/ASC/code/utils/fparam.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# *************************************************
+# Author: guwenqi
+# Last modified:
+# Email: hey_xiaoqi@163.com
+# Filename: fparam.py
+# Description: feature params setting
+# *************************************************
+
+# **********************************************
+# * mbe feature setting, global feature setting
+# **********************************************
+
+# mbe order
+MBE_ORDER = 64
+# frame length in second
+MBE_FRAME_LENGTH = 0.04
+# frame shift in second
+MBE_FRAME_SHIFT = 0.02
+# mbe context length in frames
+MBE_SEGMENT_LENGTH = 100
+# mbe context shift in frames while training model
+MBE_SEGMENT_SHIFT_TRAIN = 33
+# mbe context shift in frames in test phase
+MBE_SEGMENT_SHIFT_TEST = 10
+# mbe model operating sample rate
+MBE_SAMPLE_RATE = 16000
+
+# **********************************************
+# * mfcc feature setting, global feature setting
+# **********************************************
+
+# mfcc order
+MFCC_ORDER = 26
